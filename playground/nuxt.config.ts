a0acb4c0a08d374f082da77b59f91cdfe3fe41a3
rrd108 <rrd@webmania.cc>
2024-03-29 11:59:56 +0100

    generated module

---
+++ playground/nuxt.config.ts
@@ -0,0 +1,5 @@
+export default defineNuxtConfig({
+  modules: ["../src/module"],
+  nuxtApiShield: {},
+  devtools: { enabled: true },
+});
---
+++ test/fixtures/basic/nuxt.config.ts
@@ -0,0 +1,5 @@
+import nuxtApiShield from "../../../src/module";
+
+export default defineNuxtConfig({
+  modules: [nuxtApiShield],
+});
