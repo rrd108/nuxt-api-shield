9fc0ce923924822830d96520b99eb7d1cc3fa689
rrd108 <rrd@webmania.cc>
2024-03-30 12:03:18 +0100

    chore: add comments

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,11 +1,15 @@
 import { describe, it, expect } from "vitest";
 import { fileURLToPath } from "node:url";
 import { setup, $fetch } from "@nuxt/test-utils/e2e";
+import { beforeEach } from "vitest";
+
+beforeEach(async () => {
+  // TODO await useStorage().clear();
+});
 
 describe("shield", async () => {
   await setup({
     rootDir: fileURLToPath(new URL("./fixtures/basic", import.meta.url)),
-    // TODO empty the storage before each test
   });
 
   it("respond to api call 2 times (limit.max, limit.duration)", async () => {
@@ -33,8 +37,8 @@ describe("shield", async () => {
       expect(typedErr.statusMessage).toBe("Too Many Requests");
     }
 
-    // here we should wait for the ban to be lifted
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // here we should wait for the ban to expire
+    await new Promise((resolve) => setTimeout(resolve, 3000)); // limit.ban
     const response = await $fetch("/api/example", { method: "GET" });
     expect(response.name).toBe("Gauranga");
   });
